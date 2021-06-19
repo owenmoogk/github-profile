bcce8e530132be1e67e628aff0ceed98ee339551
owenmoogk <owenmoogk@gmail.com>
2021-06-19 16:49:35 -0400

    added api key deletion :octocat: :key:

--- src/components/homepage/APIKey.js
+++ src/components/homepage/APIKey.js
@@ -5,12 +5,17 @@ export default function APIKey(props) {
 	
 	const matched = useRouteMatch(['/'])
 	const [message, setMessage] = useState()
-	const [currentToken, setCurrentToken] = useState()
 
 	function submitData(e) {
 		e.preventDefault()
 		var tokenValue = document.getElementById('apiKeyInput').value
 		setMessage('')
+
+		if (tokenValue === ''){
+			localStorage.setItem('token', '')
+			props.getRateLimit()
+			return
+		}
 		
 		// testing if valid key
 		fetch("https://api.github.com/rate_limit", {
@@ -22,8 +27,6 @@ export default function APIKey(props) {
 			.then(json => {
 				if (json.rate){
 					localStorage.setItem('token', 'token '+tokenValue)
-					setCurrentToken(tokenValue)
-					document.getElementById('apiKeyInput').value = ''
 					props.getRateLimit()
 				}
 				else{
@@ -33,18 +36,15 @@ export default function APIKey(props) {
 	}
 
 	useEffect(() => {
-		setCurrentToken(localStorage.getItem('token').replace('token ', ''))
+		document.getElementById('apiKeyInput').value = localStorage.getItem('token') ? localStorage.getItem('token').replace('token ', '') : null
 	}, [])
 
 	if (matched && matched.isExact) {
 		return (
 			<form onSubmit={(e) => submitData(e)}>
-				<span>Current Token: <br/>{currentToken ? currentToken : 'None'}</span>
-				<br/>
 				<input type='text' placeholder='ghp_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx ' id='apiKeyInput' />
-				<br/><br/>
 				<p style={{marginTop: 0}}>{message}</p>
-				<input type='submit'/>
+				<input type='submit' />
 			</form>
 		)
 	}
