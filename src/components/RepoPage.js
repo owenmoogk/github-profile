ae0903e7b38c93dc9cefc3ac7ead32c9691432dc
owenmoogk <owenmoogk@gmail.com>
2021-06-19 22:56:54 -0400

    react warnings and formatting :warning:

--- src/components/RepoPage.js
+++ src/components/RepoPage.js
@@ -40,18 +40,18 @@ export default function RepoPage(props) {
 		return (
 			<p className='info'>
 				<span>
-					<svg aria-hidden="true" viewBox="0 0 16 16" version="1.1" data-view-component="true" height="20" width="16" class="octicon octicon-repo-forked" style={{ fill: 'currentcolor' }}>
-						<path fill-rule="evenodd" d="M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 100-1.5.75.75 0 000 1.5z"></path>
+					<svg aria-hidden="true" viewBox="0 0 16 16" version="1.1" data-view-component="true" height="20" width="16" className="octicon octicon-repo-forked" style={{ fill: 'currentcolor' }}>
+						<path fillRule="evenodd" d="M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 100-1.5.75.75 0 000 1.5z"></path>
 					</svg> {data.forks}
 				</span>
 				<span>
-					<svg aria-hidden="true" viewBox="0 0 16 16" version="1.1" data-view-component="true" height="20" width="16" class="octicon octicon-star mr-1" style={{ fill: 'currentcolor' }}>
-						<path fill-rule="evenodd" d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"></path>
+					<svg aria-hidden="true" viewBox="0 0 16 16" version="1.1" data-view-component="true" height="20" width="16" className="octicon octicon-star mr-1" style={{ fill: 'currentcolor' }}>
+						<path fillRule="evenodd" d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"></path>
 					</svg> {data.stargazers_count}
 				</span>
 				<span>
-					<svg aria-hidden="true" viewBox="0 0 16 16" version="1.1" data-view-component="true" height="20" width="16" style={{ fill: 'currentcolor' }} class="octicon octicon-eye">
-						<path fill-rule="evenodd" d="M1.679 7.932c.412-.621 1.242-1.75 2.366-2.717C5.175 4.242 6.527 3.5 8 3.5c1.473 0 2.824.742 3.955 1.715 1.124.967 1.954 2.096 2.366 2.717a.119.119 0 010 .136c-.412.621-1.242 1.75-2.366 2.717C10.825 11.758 9.473 12.5 8 12.5c-1.473 0-2.824-.742-3.955-1.715C2.92 9.818 2.09 8.69 1.679 8.068a.119.119 0 010-.136zM8 2c-1.981 0-3.67.992-4.933 2.078C1.797 5.169.88 6.423.43 7.1a1.619 1.619 0 000 1.798c.45.678 1.367 1.932 2.637 3.024C4.329 13.008 6.019 14 8 14c1.981 0 3.67-.992 4.933-2.078 1.27-1.091 2.187-2.345 2.637-3.023a1.619 1.619 0 000-1.798c-.45-.678-1.367-1.932-2.637-3.023C11.671 2.992 9.981 2 8 2zm0 8a2 2 0 100-4 2 2 0 000 4z"></path>
+					<svg aria-hidden="true" viewBox="0 0 16 16" version="1.1" data-view-component="true" height="20" width="16" style={{ fill: 'currentcolor' }} className="octicon octicon-eye">
+						<path fillRule="evenodd" d="M1.679 7.932c.412-.621 1.242-1.75 2.366-2.717C5.175 4.242 6.527 3.5 8 3.5c1.473 0 2.824.742 3.955 1.715 1.124.967 1.954 2.096 2.366 2.717a.119.119 0 010 .136c-.412.621-1.242 1.75-2.366 2.717C10.825 11.758 9.473 12.5 8 12.5c-1.473 0-2.824-.742-3.955-1.715C2.92 9.818 2.09 8.69 1.679 8.068a.119.119 0 010-.136zM8 2c-1.981 0-3.67.992-4.933 2.078C1.797 5.169.88 6.423.43 7.1a1.619 1.619 0 000 1.798c.45.678 1.367 1.932 2.637 3.024C4.329 13.008 6.019 14 8 14c1.981 0 3.67-.992 4.933-2.078 1.27-1.091 2.187-2.345 2.637-3.023a1.619 1.619 0 000-1.798c-.45-.678-1.367-1.932-2.637-3.023C11.671 2.992 9.981 2 8 2zm0 8a2 2 0 100-4 2 2 0 000 4z"></path>
 					</svg> {data.subscribers_count}
 				</span>
 				<br />
@@ -59,7 +59,7 @@ export default function RepoPage(props) {
 					data.fork
 						? <span>
 							<svg aria-hidden="true" viewBox="0 0 16 16" version="1.1" data-view-component="true" height="16" width="16" className="octicon octicon-repo-forked" style={{ fill: 'currentcolor' }}>
-								<path fill-rule="evenodd" d="M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 100-1.5.75.75 0 000 1.5z"></path>
+								<path fillRule="evenodd" d="M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 100-1.5.75.75 0 000 1.5z"></path>
 							</svg>&nbsp;Forked from&nbsp;
 							<a style={{
 								'textDecoration': 'none'
@@ -114,10 +114,11 @@ export default function RepoPage(props) {
 							data={
 								Object.entries(languageData).map(([language, value], i) => {
 									return (
-										{ title: language, value: value, color: 
-											props.colors[language]
-												? props.colors[language].color
-												: 'grey' 
+										{
+											title: language, value: value, color:
+												props.colors[language]
+													? props.colors[language].color
+													: 'grey'
 										}
 									)
 								})
@@ -141,12 +142,14 @@ export default function RepoPage(props) {
 							Object.entries(languageData).map(([language, value], i) => {
 								return (
 									<div key={i}>
-										<span style={{ 'width': '14px', 'height': '14px', 'borderRadius': '100%', 'backgroundColor': 
-											props.colors[language]
-												? props.colors[language].color
-												: 'grey'
-											, 
-										'display': 'inline-block', 'top': '1px', 'position': 'relative' }}></span>&nbsp;
+										<span style={{
+											'width': '14px', 'height': '14px', 'borderRadius': '100%', 'backgroundColor':
+												props.colors[language]
+													? props.colors[language].color
+													: 'grey'
+											,
+											'display': 'inline-block', 'top': '1px', 'position': 'relative'
+										}}></span>&nbsp;
 										<span className='language'>{language}</span>
 									</div>
 								)
@@ -197,7 +200,7 @@ export default function RepoPage(props) {
 					<div id='contributorList'>
 						{contributorData.map((user, i) => {
 							return (
-								<a key={i} href={'/'+user.login}>
+								<a key={i} href={'/' + user.login}>
 									<img src={user.avatar_url} alt='' />
 									<span> &nbsp; {user.login}</span>
 								</a>
@@ -212,10 +215,6 @@ export default function RepoPage(props) {
 					</h2>
 					{renderContributorChart()}
 				</div>
-
-				{/* <div id='data'>
-						<GithubCard data={data} colors={props.colors} emojis={props.emojis} user={username} link='external' />
-					</div> */}
 			</div>
 		)
 	}
@@ -243,7 +242,7 @@ export default function RepoPage(props) {
 					? renderComponents()
 					: null
 				}
-				<br/><br/><br/>
+				<br /><br /><br />
 
 			</div>
 		)
